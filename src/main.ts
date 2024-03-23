a6e044c8797c8ffb25590f94d8706dbd543a3168
huansky <651494458@qq.com>
2024-03-23 12:56:50 +0800

    feat:添加antd和常用插件

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import Antd from 'ant-design-vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+// 引入全局样式
+import 'ant-design-vue/dist/reset.css';
+import './style.css'
+
+const app = createApp(App);
+
+app.use(Antd);
+
+app.mount('#app');
